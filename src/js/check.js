76724d66ec04cfac7dcf39c124b26c53cef96bdd
sevryukovd <sevryukov.d@gmail.com>
2016-10-07 21:16:30 +0300

    Начал работу над заданием

--- src/js/check.js
+++ src/js/check.js
@@ -1,14 +1,32 @@
-"use strict";
+'use strict';
 
-var a; 
+var a;
 var b;
 
 function getMessage(a, b) {
-   if (typeof a == "boolean") {
-      if (a == true) {
-         return ("Переданное GIF-изображение анимировано и содержит [b] кадров");
-      } else {
-         return ("Переданное GIF-изображение не анимировано");
-      }
+  if (typeof (a) == 'boolean') {
+    if (a) {
+      return ('Переданное GIF-изображение анимировано и содержит ' + b + ' кадров');
+    } else {
+      return ("Переданное GIF-изображение не анимировано");
+    }
 
-   }
+  } else if (typeof (a) == 'number') {
+    return ('Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов');
+
+  } else if (Array.isArray(a)) {
+    var amountOfRedPoints = 0;
+    for (var i = 0; i < a.length; i++) {
+      amountOfRedPoints = amountOfRedPoints + a[i]
+    }
+    return ('Количество красных точек во всех строчках изображения:' + amountOfRedPoints);
+
+  } else if (Array.isArray(a) && Array.isArray(b)) {
+    var artifactsSquare = 0;
+
+    for (var i = 0; i < a.length; i++) {
+      artifactsSquare += a[i] * b[i];
+    }
+    return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+  }
+};
