3fd9eb31d81aec85cf662c5969741ad904eb03a1
sevryukovd <sevryukov.d@gmail.com>
2016-10-08 20:45:30 +0300

    Внес исправления

--- src/js/check.js
+++ src/js/check.js
@@ -4,22 +4,15 @@ var a;
 var b;
 
 function getMessage(a, b) {
-  if (typeof (a) == 'boolean') {
+  if (typeof (a) === 'boolean') {
     if (a) {
       return ('Переданное GIF-изображение анимировано и содержит ' + b + ' кадров');
     } else {
       return ("Переданное GIF-изображение не анимировано");
     }
 
-  } else if (typeof (a) == 'number') {
+  } else if (typeof (a) === 'number') {
     return ('Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов');
-
-  } else if (Array.isArray(a)) {
-    var amountOfRedPoints = 0;
-    for (var i = 0; i < a.length; i++) {
-      amountOfRedPoints += a[i]
-    }
-    return ('Количество красных точек во всех строчках изображения:' + amountOfRedPoints);
 
   } else if (Array.isArray(a) && Array.isArray(b)) {
     var artifactsSquare = 0;
@@ -28,5 +21,15 @@ function getMessage(a, b) {
       artifactsSquare += a[i] * b[i];
     }
     return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+  } else if (Array.isArray(a)) {
+    var amountOfRedPoints = 0;
+    for (var i = 0; i < a.length; i++) {
+      amountOfRedPoints += a[i]
+    }
+    return ('Количество красных точек во всех строчках изображения:' + amountOfRedPoints);
+
+  }
+  else {
+   return ('Переданы некорректные данные');
   }
 };
