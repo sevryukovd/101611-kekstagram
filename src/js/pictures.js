6bcd58a00dbd9c70c57a5d746249ca919147953e
sevryukovd <sevryukov.d@gmail.com>
2016-11-08 19:02:10 +0300

    Правка в forEach3

--- src/js/pictures.js
+++ src/js/pictures.js
@@ -1,26 +1,29 @@
 'use strict';
 var galleryObject = require('./gallery');
-var Picture = require('./picture');
 var load = require('./load');
+var Picture = require('./picture');
 
+var load = require('./load');
 var URL_UPLOAD_PICTURES = 'http://localhost:1507/api/pictures';
 var picturesContainer = document.querySelector('.pictures');
 var filterHidden = document.querySelector('.filters');
 
 
 var showPictures = (function() {
-  filterHidden.classList.add('hidden');
+filterHidden.classList.add('hidden');
 
 var renderPictures = function(pictureArray) {
-    var newPicture = new Picture(picture, index);
+
     pictureArray.forEach(function(picture, index) {
+      var newPicture = new Picture(picture, index);
       picturesContainer.appendChild(newPicture.element);
     });
     galleryObject.setPictures(pictureArray);
 
   };
 
-  load(URL_UPLOAD_PICTURES, renderPictures, 'jsonpCallback');
+
+load(URL_UPLOAD_PICTURES, renderPictures, 'jsonpCallback');
   filterHidden.classList.remove('hidden');
 })();
 
